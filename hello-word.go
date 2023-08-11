6e2fc3551e4bc47235ec34cb75cdf193c2fa2873
Diego Lima <lima.j.diego@gmail.com>
2023-08-11 17:36:03 -0300

    First hello world

---
+++ hello-word.go
@@ -0,0 +1,15 @@
+package main // Todo programa Go começa com uma declaração de pacote. Um programa executável sempre deve ter um pacote main.
+
+/*
+	Aqui, estamos importando o pacote fmt, que fornece funções para formatar e imprimir dados.
+	O pacote fmt é usado aqui para imprimir a saída no console.
+
+*/
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!") // Utiliza a função Println do pacote fmt para imprimir a string "Hello, World!"
+}
+
+// Execulta arquivo no terminal
+// go run hello-world.go
